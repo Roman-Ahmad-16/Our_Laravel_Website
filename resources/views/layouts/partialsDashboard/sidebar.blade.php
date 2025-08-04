c2f56c37e31b437cea38229c3b32409b0d47b6c2
Roman-Ahmad-16 <websitedesigner725@gmail.com>
2025-08-03 21:13:13 -0700

    dashboard

---
+++ resources/views/layouts/partialsDashboard/sidebar.blade.php
@@ -0,0 +1,31 @@
+<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+    <div class="sb-sidenav-menu">
+        <div class="nav">
+            <div class="sb-sidenav-menu-heading">Core</div>
+            <a class="nav-link" href="{{ url('/admin/dashboard') }}">
+                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                Dashboard
+            </a>
+
+            <div class="sb-sidenav-menu-heading">Addons</div>
+            <a class="nav-link" href="#">
+                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                Charts
+            </a>
+            <a class="nav-link" href="#">
+                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                Tables
+            </a>
+
+            <a class="nav-link" href="#">
+                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                Why Choose Us
+            </a>
+
+        </div>
+    </div>
+    <div class="sb-sidenav-footer">
+        <div class="small">Logged in as:</div>
+        Start Bootstrap
+    </div>
+</nav>
